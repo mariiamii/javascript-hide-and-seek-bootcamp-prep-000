c60d4588bda4dd0ca7d5ca9b535fad6332519271
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-09-10 00:34:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,35 +17,9 @@ function deepestChild() {
   return (gNode);
 }
 
-describe('deepestChild()', () => {
-    it('returns the most deeply nested child in #grand-node', () => {
-      console.log(deepestChild().innerHTML)
-      expect(deepestChild()).to.equal(document.querySelector('#grand-node div div div div'))
-
 function increaseRankBy(n) {
   const list = document.querySelectorAll('ul.ranked-list li')
   for (var i = 0; i < list.length; i++) {
       list[i].innerHTML = parseInt(list[i].innerHTML) + n;
     }
   }
-
-  describe('increaseRankBy(n)', () => {
-    it('increases ranks in .ranked-list by n', () => {
-      increaseRankBy(3)
-
-      const rankedLists = document.querySelectorAll('.ranked-list')
-      const firstList = rankedLists[0]
-      const secondList = rankedLists[1]
-
-      let children = firstList.children
-      let start = 1
-      for (let i = 0, l = children.length; i < l; i++) {
-        expect(parseInt(children[i].innerHTML)).to.equal(start + i + 3)
-      }
-
-      children = secondList.children
-      start = 12
-
-      for (let i = 0, l = children.length; i < l; i++) {
-        expect(parseInt(children[i].innerHTML)).to.equal(start - i + 3)
-     
