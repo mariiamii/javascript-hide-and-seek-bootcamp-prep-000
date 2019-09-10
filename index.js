8497ad1ecda3b09273b87557b3aa93e04c1a9bc2
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-09-10 00:35:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  let gNode = document.querySelector('#grand-node')
+  let nextNode = gNode.children[0];
+  
+  while (nextNode) {
+    gNode = nextNode;
+    nextNode = gNode.children[0];
+  }
+  return (gNode);
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll('ul.ranked-list li')
+  for (var i = 0; i < list.length; i++) {
+      list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+    }
+  }
