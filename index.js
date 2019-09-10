432d43acd703dddfbe6759f79b185b5da31f2a02
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-09-10 00:22:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-
 function increaseRankBy(n) {
   const list = document.quarySelectorAll('ul.ranked-list li')
   for (var i = 0; i < list.length; i++) {
